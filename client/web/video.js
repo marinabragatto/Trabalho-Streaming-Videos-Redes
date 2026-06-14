68832680559c312a502c2ab6fe9c95a0c772e1e1
thaisssssssssss <thaisgomes1585@gmail.com>
2026-06-13 23:19:29 -0300

    fix: mudanca de qualidade sincronizada

--- client/web/video.js
+++ client/web/video.js
@@ -13,10 +13,16 @@ const qualitySelect = document.getElementById("quality-select");
 const quality = params.get("quality") || "1080";
 
 qualitySelect.value = quality;
+
+//troca a qualidade e salva alguns atributos com informacoes do video anterior 
 qualitySelect.addEventListener("change", () => {
+    console.log("trocando de qualidade");
+
+    localStorage.setItem("lastVideoId", videoId); 
+    localStorage.setItem("lastVideoTime", video.currentTime);
+    localStorage.setItem("wasPlaying", !video.paused);
 
     params.set("quality", qualitySelect.value);
-
     window.location.search = params.toString();
 });
 
@@ -39,6 +45,10 @@ let currentAudioSegment = 0;
 let appendingVideo = false;
 let appendingAudio = false;
 
+// Controle para não tentar update enquanto esta restaurando o video 
+let restoring = false;
+
+
 // Carrega o manifest.json pelo backend Go
 async function loadManifest() {
     const response = await fetch(`/manifest?id=${videoId}`);
@@ -60,7 +70,8 @@ function startMediaSource() {
     // O src do vídeo agora vira blob, não /stream direto
     video.src = URL.createObjectURL(mediaSource);
 
-    mediaSource.addEventListener("sourceopen", () => {
+    // O async permite usar await dentro dessa funcao
+    mediaSource.addEventListener("sourceopen", async () => {
     
         console.log("MediaSource aberto");
 
@@ -71,9 +82,14 @@ function startMediaSource() {
         const audioCodec = 'audio/mp4; codecs="mp4a.40.2"';
 
         videoBuffer = mediaSource.addSourceBuffer(videoCodec);
-        audioBuffer = mediaSource.addSourceBuffer(audioCodec);
+        audioBuffer = mediaSource.addSourceBuffer(audioCodec);        
 
+        // entra nessa funcao sempre que o vídeo avanca no tempo
         video.addEventListener("timeupdate", () => {
+            console.log("ADD EVENT LISTENER TIMEUPDATE");
+
+            if(restoring) return; //nao permite carregar seguimentos enquanto esta restaurando
+
             const buffered = video.buffered.length > 0 ? video.buffered.end(0) : 0;
             const ahead = buffered - video.currentTime;
 
@@ -88,31 +104,94 @@ function startMediaSource() {
             }
         });
 
+        // esse evento acontece quando o vídeo trava esperando dados
         video.addEventListener("waiting", () => {
+            console.log("ADD EVENT LISTENER WAITING");
+
+            if(restoring) return; //nao permite carregar seguimentos enquanto esta restaurando
+
             appendNextVideoSegment();
             appendNextAudioSegment();
-        });
-
-        // Só reseta o flag de "ocupado", não chama o próximo segmento
-        videoBuffer.addEventListener("updateend", () => {
-            appendingVideo = false;
-            tryEndStream();
-        });
-
-        audioBuffer.addEventListener("updateend", () => {
-            appendingAudio = false;
-            tryEndStream();
         });
 
         videoBuffer.addEventListener("error", (e) => console.error("Erro no videoBuffer:", e));
         audioBuffer.addEventListener("error", (e) => console.error("Erro no audioBuffer:", e));
         video.addEventListener("error", () => console.error("Erro no elemento video:", video.error));
 
-        // Carga inicial — sem isso o vídeo não começa pois o timeupdate
-        // só dispara depois que o vídeo já está tocando
-        appendNextVideoSegment();
-        appendNextAudioSegment();
-    });
+        
+
+        // busca informacoes do video anterior como id, tempo em que estava e se estava tocando
+        const savedVideoId = localStorage.getItem("lastVideoId");
+        const savedTime = Number(localStorage.getItem("lastVideoTime"));
+        const wasPlaying = localStorage.getItem("wasPlaying") === "true";       
+
+        //se o evento foi apenas uma troca de qualidade e nao uma troca de video
+        //entra nessa funcao pra carregar os seguimentos ate o tempo em que estava 
+        //antes da troca
+        if (savedVideoId === videoId && savedTime > 0) {
+            restoring = true;
+
+            console.log("CARREGANDO INIT");
+
+            await appendNextVideoSegment(); // init do vídeo
+            await appendNextAudioSegment(); // init do audio
+
+            //calculo estimado para descobrir qual seguimentos estava tocando
+            const segmentDuration = 4;
+            const segmentIndex = Math.floor(savedTime / segmentDuration);
+
+            currentVideoSegment = segmentIndex;
+            currentAudioSegment = segmentIndex; 
+            
+
+            //carrega os 4 proximos seguimentos a partir do definido pelo index
+            //"carga inicial"
+            for (let i = 0; i < 6; i++) {
+                console.log("CARREGANDO OUTROS SEGUIMENTOS");
+
+                await appendNextVideoSegment();
+                await appendNextAudioSegment();
+            }
+
+            
+            // Cria uma Promise para esperar o seek terminar.
+            // O seek é o ato de mudar video.currentTime para outro ponto.
+            const seekPromise = new Promise((resolve) => {
+                // Escuta o evento "seeked".
+                // Esse evento acontece quando o navegador terminou de ir
+                // para o tempo solicitado.
+                video.addEventListener("seeked", function onSeeked() {
+                    console.log("Seeked para:", video.currentTime);
+                    
+                    video.removeEventListener("seeked", onSeeked);
+
+                    // Finaliza a Promise e libera o await seekPromise abaixo.
+                    resolve();
+                });
+            });
+
+            video.currentTime = savedTime;
+
+            //espera o evento seeked acontecer
+            await seekPromise;
+
+            if (wasPlaying) {
+                await video.play();
+            }
+
+            localStorage.removeItem("lastVideoTime");
+            localStorage.removeItem("wasPlaying");
+
+            restoring = false;
+        }        
+        else {
+            // Caso seja um video novo:
+            // Carga inicial — sem isso o vídeo não começa pois o timeupdate
+            // só dispara depois que o vídeo já está tocando
+            await appendNextVideoSegment();
+            await appendNextAudioSegment();
+        }
+    });        
 }
 
 // Adiciona o próximo segmento de vídeo ao buffer de vídeo
@@ -128,7 +207,20 @@ async function appendNextVideoSegment() {
     appendingVideo = true;
 
     const data = await fetchSegment(segmentName);
-    videoBuffer.appendBuffer(data);
+
+    // Cria uma Promise para esperar o appendBuffer terminar.
+    return new Promise((resolve) => {
+        videoBuffer.addEventListener("updateend", function handler() {
+            videoBuffer.removeEventListener("updateend", handler);
+
+            appendingVideo = false;
+            tryEndStream();
+
+            resolve();
+        });
+
+        videoBuffer.appendBuffer(data);
+    });
 }
 
 // Adiciona o próximo segmento de áudio ao buffer de áudio
@@ -144,7 +236,20 @@ async function appendNextAudioSegment() {
     appendingAudio = true;
 
     const data = await fetchSegment(segmentName);
-    audioBuffer.appendBuffer(data);
+
+    // Cria uma Promise para esperar o appendBuffer terminar.
+    return new Promise((resolve) => {
+        audioBuffer.addEventListener("updateend", function handler() {
+            audioBuffer.removeEventListener("updateend", handler);
+
+            appendingAudio = false;
+            tryEndStream();
+
+            resolve();
+        });
+
+        audioBuffer.appendBuffer(data);
+    });
 }
 
 // Busca um segmento no backend Go.
@@ -155,7 +260,7 @@ async function fetchSegment(segmentName) {
     `&quality=${quality}` +
     `&segment=${segmentName}`;
     console.log("Carregando:", url);
-
+    
     const response = await fetch(url);
 
     if (!response.ok) {
